8018731b32baac4e5d1439246ab0a55da6383b1d
ersagun <ersagunyalcintepe@gmail.com>
2016-11-16 20:08:20 +0100

    database changed

--- target/TPWeb1-1.0-SNAPSHOT/js/inscription.js
+++ target/TPWeb1-1.0-SNAPSHOT/js/inscription.js
@@ -1,8 +1,4 @@
-/* 
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
+
 $(document).ready(function () {
     if($('#particulier').is(':checked') || $('#entreprise').is(':checked') ){ 
      window.location.reload(true)    
